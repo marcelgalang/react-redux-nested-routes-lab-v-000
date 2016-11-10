02249592e559ae5e0c938b325052cc5ce847aa92
ipc103 <iancandysemail@gmail.com>
2016-11-10 15:43:33 -0500

    add nightwatch

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,17 @@
 import expect from 'expect';
+import nightwatch from 'nightwatch';
 
-describe('index', function() {
-  it('runs', function() {
-    expect(true).toEqual(true)
+
+describe('Visiting /pets/:id', function() {
+  let client = nightwatch.initClient({silent: true});
+  let browser = client.api();
+
+  it('displays info about one pet', function(done) {
+    browser
+      .url('/pets/1')
+      .waitForElementVisible('h2', 1000)
+      .assert.containsText('')
+
+    done();
   })
 })
