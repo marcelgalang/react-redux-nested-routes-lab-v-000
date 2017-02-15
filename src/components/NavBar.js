1bca20128ed44405ba2e3cd1246648751df0770c
Luke Ghenco <lukeghenco@gmail.com>
2017-02-15 08:05:29 -0500

    clean up starter code for components and containers

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
 export default (props) => {
   return(
     <nav className="navbar navbar-default">
-      <Link className="navbar-brand" to={props.href}>{props.title}</Link>
+      <Link 
+        className="navbar-brand" 
+        to={props.href}>
+        {props.title}
+      </Link>
     </nav>
   )
-}
+};
--- src/containers/PetsShow.js
+++ src/containers/PetsShow.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-export default () => {
-
-  return (
-    <div className="col-md-8">
-    </div>)
-}
+export default (props) => {
+  return <div className="col-md-8"></div>;
+};
