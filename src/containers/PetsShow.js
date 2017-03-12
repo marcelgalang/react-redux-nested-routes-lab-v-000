d0b835811ba2bfaedc98cba1a37cb95761312a7f
github username <github email address>
2017-03-12 09:18:55 -0400

    Done.

--- src/containers/PetsShow.js
+++ src/containers/PetsShow.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-export default (props) => {
-  return <div className="col-md-8"></div>;
+const PetsShow = (props) => {
+  const pet = props.pet;
+
+  return (
+    <div className="col-md-8">
+      <h2>{pet.name}</h2>
+      <p>{pet.description}</p>
+    </div>
+  );
 };
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    pet: state.pets.find(pet => pet.id == ownProps.routeParams.id)
+  };
+};
+
+export default connect(mapStateToProps)(PetsShow);
